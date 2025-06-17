428b77df0c7b2c1c18233b0af559f9c8da53187c
iamkdao <daominhkhoi64@gmail.com>
2025-06-17 10:53:41 +0700

    Added interceptors

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,2 +1,27 @@
 import axios from 'axios'
 
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8000',
+    timeout: 5000,
+})
+
+axiosInstance.interceptors.request.use(
+    function (config) {
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error)
+    }
+)
+
+axiosInstance.interceptors.response.use(
+    function (response) {
+        return response;
+    },
+    function (error) {
+        console.error('API Error:', error);
+        return Promise.reject(error);
+    }
+)
+
+export default axiosInstance;
--- frontend/src/services/WeatherAPI.js
+++ frontend/src/services/WeatherAPI.js
@@ -1,8 +1,13 @@
+import axiosInstance from "../api/axiosInstance";
 const API_KEY = '052a005fe3729f0026a06e48a12bd0a0'; // Get from OpenWeatherMap
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export async function getWeatherByCity(city) {
-    const res = await fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`);
-    if (!res.ok) throw new Error(res.statusText);
-    return await res.json();
+    try {
+        const res = await axiosInstance.get(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`)
+        return res.data;
+    }
+    catch (error) {
+        throw error;
+    }
 }
